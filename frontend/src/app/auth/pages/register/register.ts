4bfd99c64eade9827671834d394949d5606abe0c
faguilar02 <daguilarv1@upao.edu.pe>
2025-10-26 18:41:22 -0500

    correr app en hot reaload

---
+++ frontend/src/app/auth/pages/register/register.ts
@@ -0,0 +1,48 @@
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormUtils } from '../../../utils/form-utils';
+import { AuthService } from '../../service/auth-service';
+
+@Component({
+  selector: 'app-register',
+  imports: [ReactiveFormsModule],
+  templateUrl: './register.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Register {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  formUtils = FormUtils;
+  hasError = signal(false);
+
+  loginForm = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(9)]],
+    password: ['', [Validators.required, Validators.minLength(9)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const { username, password } = this.loginForm.value;
+
+    this.authService
+      .login(username!, password!)
+      .subscribe((isAuthenticated) => {
+        if (isAuthenticated) {
+          this.router.navigateByUrl('/');
+          return;
+        }
+
+        this.hasError.set(true);
+        setTimeout(() => {
+          this.hasError.set(false);
+        }, 2000);
+      });
+  }
+}
--- frontend/src/app/auth/routes/auth-routes.ts
+++ frontend/src/app/auth/routes/auth-routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Login } from '../pages/login/login';
+import { Register } from '../pages/register/register';
 
 export const authRoutes: Routes = [
   {
     path: 'login',
     component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
   },
   {
     path: '**',
